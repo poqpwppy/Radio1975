590701f502ebd94ab4c903b938291dd81c63b44c
Glitch (sturdy-dear-tuberose) <none>
2021-06-08 17:03:50 +0000

    💁🏪 Checkpoint ./server.js:21271476/336 ./watch.json:21271476/242

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+//Bot
 const keepAlive = require('./server1.js');
 const Eco = require("quick.eco");
 const Discord = require("discord.js");

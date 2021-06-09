6a98ab94e1d5c8c09c99857f0e902b52dc4948c6
Quak <64719091+quak910@users.noreply.github.com>
2021-06-09 20:01:59 +0700

    Create play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,111 +1,139 @@
-const ytdl = require('ytdl-core-discord');
-var scrapeYt = require("scrape-yt");
-const discord = require('discord.js')
+const { Util, MessageEmbed } = require('discord.js');
+const { GOOGLE_API_KEY } = require('../../config');
+const YouTube = require("simple-youtube-api");
+const youtube = new YouTube(GOOGLE_API_KEY);
+const ytdl = require('ytdl-core');
 
 module.exports = {
-  name: "play",
-  aliases: ["p"],
+    config: {
+        name: 'play',
+        description: 'Chơi nhạc',
+        aliases: ["p"],
+        category: "music",
+        usage: '[song (name | link)]',
+        accessableby: "everyone"
+    },
+    run: async (bot, message, args, ops) => {
+        if (!args[0]) return message.channel.send("**Please Enter Song Name Or Link!**")
+        args = message.content.split(' ');
+        const searchString = args.slice(1).join(' ');
+        const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
 
-  run: async (client, message, args) => {
-    if (!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('Bạn không có quyền')
-    if(!args[0]) return message.channel.send('Bạn chưa  cung cấp bài để phát!')
-    let channel = message.member.voice.channel;
-    if(!channel) return message.channel.send('Bạn phải ở trong kênh thoại!')
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
 
-    if (!channel.permissionsFor(message.client.user).has("CONNECT")) return message.channel.send('Tôi không có quyền để tham gia kênh âm thoại')
-    if (!channel.permissionsFor(message.client.user).has("SPEAK"))return message.channel.send('Tôi không có quyền để nói trong kênh')
-    
-    let bot = message.guild.me.voice.channel
-    if (!bot) return channel.join()
+        const permissions = channel.permissionsFor(message.client.user);
+        if (!permissions.has('CONNECT')) return message.channel.send('I cannot connect to your voice channel, make sure I have the proper permissions!');
+        if (!permissions.has('SPEAK')) return message.channel.send('I cannot speak in this voice channel, make sure I have the proper permissions!');
 
-    const server = message.client.queue.get(message.guild.id);
-    let video = await scrapeYt.search(args.join(' '))
-    let result = video[0]
+        if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
+            const playlist = await youtube.getPlaylist(url);
+            const videos = await playlist.getVideos();
 
-    const song = {
-        id: result.id,
-        title: result.title,
-        duration: result.duration,
-        thumbnail: result.thumbnail,
-        upload: result.uploadDate,
-        views: result.viewCount,
-        requester: message.author,
-        channel: result.channel.name,
-        channelurl: result.channel.url
-      };
-
-    var date = new Date(0);
-    date.setSeconds(song.duration); // specify value for SECONDS here
-    var timeString = date.toISOString().substr(11, 8);
-
-      if (server) {
-        server.songs.push(song);
-        console.log(server.songs);
-        let embed = new discord.MessageEmbed()
-        .setTitle('Đã thêm vào hàng chờ!')
-        .setColor('#00fff1')
-        .addField('Tên', song.title, true)
-        .setThumbnail(song.thumbnail)
-        .addField('Số người xem', song.views, true)
-        .addField('Reqeusted bởi', song.requester, true)
-        .addField('Thời gian', timeString, true)
-        return message.channel.send(embed)
-    }
-
-    const queueConstruct = {
-        textChannel: message.channel,
-        voiceChannel: channel,
-        connection: null,
-        songs: [],
-        volume: 2,
-        playing: true
-    };
-    message.client.queue.set(message.guild.id, queueConstruct);
-    queueConstruct.songs.push(song);
-
-
-    const play = async song => {
-        const queue = message.client.queue.get(message.guild.id);
-        if (!song) {
-            queue.voiceChannel.leave();
-            message.client.queue.delete(message.guild.id);
-            message.channel.send('Không có gì để phát, tôi thoát đây!')
-            return;
+            for (const video of Object.values(videos)) {
+                const video2 = await youtube.getVideoByID(video.id);
+                await handleVideo(video2, message, channel, true);
+            }
+            return message.channel.send(`**Playlist \`${playlist.title}\` has been added to the queue!**`);
+        } else {
+            try {
+                var video = await youtube.getVideo(url);
+            } catch (error) {
+                try {
+                    var videos = await youtube.searchVideos(searchString, 1);
+                    var video = await youtube.getVideoByID(videos[0].id);
+                } catch (err) {
+                    console.error(err)
+                    return message.channel.send('❌ **Không có kết quả!**')
+                }
+            }
+            return handleVideo(video, message, channel);
         }
+            async function handleVideo(video, message, channel, playlist = false) {
+                const serverQueue = ops.queue.get(message.guild.id);
+                const songInfo = await ytdl.getInfo(video.id);
+                const song = {
+                    id: video.id,
+                    title: Util.escapeMarkdown(video.title),
+                    url: `https://www.youtube.com/watch?v=${video.id}`,
+                    thumbnail: `https://i.ytimg.com/vi/${video.id}/maxresdefault.jpg`,
+                    duration: video.duration,
+                    time: songInfo.videoDetails.lengthSeconds
+                };
 
-        const dispatcher = queue.connection.play(await ytdl(`https://youtube.com/watch?v=${song.id}`, {
-            filter: format => ['251'],
-            highWaterMark: 1 << 25
-        }), {
-            type: 'opus'
-        })
-            .on('finish', () => {
-                queue.songs.shift();
-                play(queue.songs[0]);
-            })
-            .on('error', error => console.error(error));
-        dispatcher.setVolumeLogarithmic(queue.volume / 5);
-        let noiceEmbed = new discord.MessageEmbed()
-        .setTitle('Bắt đầu phát')
-        .setThumbnail(song.thumbnail)
-        .addField('Tên', song.title, true)
-        .addField('Requested bởi', song.requester, true)
-        .addField('Số người xem', song.views, true)
-        .addField('Thời hạn', timeString, true)
-        queue.textChannel.send(noiceEmbed);
-    };
+                if (serverQueue) {
+                    serverQueue.songs.push(song);
+                    if (playlist) return undefined;
+                    else {
+                        const sembed = new MessageEmbed()
+                            .setTitle('Đã thêm vào hàng chờ!')
+                            .setColor('#00fff1')
+                            .addField('Tên', song.title, true)
+                            .setThumbnail(song.thumbnail)
+                            .addField('Số người xem', song.views, true)
+                            .addField('Reqeusted bởi', song.requester, true)
+                            .addField('Thời gian', timeString, true)
+                            .setFooter(message.member.displayName, message.author.displayAvatarURL());
+                        message.channel.send(sembed)
+                    }
+                    return undefined;
+                }
 
-    
-    
-    try {
-        const connection = await channel.join();
-        queueConstruct.connection = connection;
-        play(queueConstruct.songs[0]);
-    } catch (error) {
-        console.error(`I could not join the voice channel`);
-        message.client.queue.delete(message.guild.id);
-        await channel.leave();
-        return message.channel.send(`I could not join the voice channel: ${error}`);
+                const queueConstruct = {
+                    textChannel: message.channel,
+                    voiceChannel: channel,
+                    connection: null,
+                    songs: [],
+                    volume: 2,
+                    playing: true,
+                    loop: false,
+                };
+                ops.queue.set(message.guild.id, queueConstruct);
+                queueConstruct.songs.push(song);
+                try {
+                    const connection = await channel.join();
+                    queueConstruct.connection = connection;
+                    play(queueConstruct.songs[0]);
+                } catch (error) {
+                    console.error(`Tôi không thể tham gia kênh thoại: ${error.message}`);
+                    ops.queue.delete(message.guild.id);
+                    await channel.leave();
+                    return message.channel.send(`Tôi không thể tham gia kênh thoại: ${error.message}`);
+                }
+            };
+            async function play(song) {
+                const queue = ops.queue.get(message.guild.id);
+                if (!song) {
+                    queue.voiceChannel.leave();
+                    ops.queue.delete(message.guild.id);
+                    return;
+                };
+
+                let npmin = Math.floor(song.time / 60);
+                let npsec = song.time - npmin * 60
+                let np = `${npmin}:${npsec}`.split(' ')
+
+                const dispatcher = queue.connection.play(ytdl(song.url, { highWaterMark: 1 << 20, quality: "highestaudio" }))
+                    .on('finish', () => {
+                        if (queue.loop) {
+                            queue.songs.push(queue.songs.shift());
+                            return play(queue.songs[0]);
+                        }
+                        queue.songs.shift();
+                        play(queue.songs[0]);
+                    })
+                    .on('error', error => console.error(error));
+                dispatcher.setVolumeLogarithmic(queue.volume / 5);
+                const embed = new MessageEmbed()
+                    .setColor("FFCCBC")
+                    .setTitle('Bắt đầu phát')
+                    .setThumbnail(song.thumbnail)
+                    .addField('Tên', song.title, true)
+                    .addField('Requested bởi', song.requester, true)
+                    .addField('Số người xem', song.views, true)
+                    .addField('Thời hạn', timeString, true)
+                    .setFooter(`Requested by: ${message.member.displayName}`, message.author.displayAvatarURL());
+                queue.textChannel.send(embed);
+            };
     }
-  }
-}  
+};

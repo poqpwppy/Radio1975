50562c73444dadbf3f09b9ff19a246b85dec3825
Quak <64719091+quak910@users.noreply.github.com>
2021-06-15 20:37:04 +0700

    Update play.js

--- commands/music/play.js
+++ commands/music/play.js
@@ -68,7 +68,7 @@ module.exports = {
 
     const play = async song => {
         const queue = message.client.queue.get(message.guild.id);
-        if (song) {
+        if (!song) {
             queue.voiceChannel.leave();
             message.client.queue.delete(message.guild.id);
             message.channel.send('Không có gì để phát, tôi thoát đây!')

718d591f53e65c40954bc4757e67d4a58c8d8ebf
Quak <64719091+quak910@users.noreply.github.com>
2021-06-24 21:12:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 exports.Prefix = `R75!`;//your prefix for bot
-exports.Token = `ODUxODQ1OTc4MTk3NDU5MDE0.YL-NYw.EOC8VdwXuw4UJQtjJT1JJI6V_zs`;//your token 
+exports.Token = ``;//your token 
 exports.Color = `FFCCBC`;//color   of embed
-exports.Ytapi = `AIzaSyAM1JpfbD4o_C4HOhCfSHQMRrA3sMvjorE`;
+exports.Ytapi = ``;
 exports.Maxplaylistsize = 10;
 exports.Staytime = 60;
 exports.Volume = 100;
 exports.Pruning = false;
 exports.Locale = `en`;
-exports.Mongo = `mongodb+srv://quak:09102006@cluster0.nff2w.mongodb.net/quak?retryWrites=true&w=majority`;
-exports.admins = ["479255769188007938"];
+exports.Mongo = ``;
+exports.admins = [""];
 exports.countChannel = "851840956537765938";
